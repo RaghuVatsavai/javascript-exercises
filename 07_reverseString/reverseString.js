82d32182970cbe224bdcc9a22ad99d4b2f608de7
Raghu Vatsavai <48869528+RaghuVatsavai@users.noreply.github.com>
2025-09-10 21:57:05 +0100

    Complete exercises 6 & 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrString = string.split("");
+    return arrString.reverse().join("");
 };
 
 // Do not edit below this line
