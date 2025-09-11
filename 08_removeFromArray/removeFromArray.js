d645dfd960b30f3801fa11bda7e938ce17018012
Raghu Vatsavai <48869528+RaghuVatsavai@users.noreply.github.com>
2025-09-11 12:05:35 +0100

    Finish exercises on loops & arrays

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    for (const arg of theArgs) {
+        while (arr.includes(arg)) {
+          let idx = arr.findIndex(item => item === arg);
+            arr.splice(idx, 1);  
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let celsius = (fahren - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahren = (celsius * 9 / 5) + 32;
+  return Math.round(fahren * 10) / 10;
 };
 
 // Do not edit below this line
